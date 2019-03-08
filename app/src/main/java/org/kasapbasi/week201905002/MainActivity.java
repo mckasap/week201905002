f897febd840987ae6c7c3791fd07dceba723fc04
mckasap <mckasap@gmail.com>
2019-03-08 15:29:50 +0300

    ilk mesaj

--- app/src/main/java/org/kasapbasi/week201905002/MainActivity.java
+++ app/src/main/java/org/kasapbasi/week201905002/MainActivity.java
@@ -2,12 +2,43 @@ package org.kasapbasi.week201905002;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private SeekBar sb;
+    private ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sb= (SeekBar) findViewById(R.id.seekBar);
+        lv= (ListView) findViewById(R.id.ListView);
+
+        sb.setMax(20);
+        sb.setProgress(10);
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                Log.i("SeekBarValue",String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
