344f99fcd2841dce20a50033864d5e2ec1e7b279
mckasap <mckasap@gmail.com>
2019-03-08 15:54:01 +0300

    ilk mesaj

--- app/src/main/java/org/kasapbasi/week201905002/MainActivity.java
+++ app/src/main/java/org/kasapbasi/week201905002/MainActivity.java
@@ -3,14 +3,35 @@ package org.kasapbasi.week201905002;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private SeekBar sb;
     private ListView lv;
+
+    private void update(){
+
+
+        ArrayList<String> liste= new ArrayList<>();
+
+        for(int i=0;i<20;i++){
+            liste.add(String.valueOf(i*sb.getProgress()));
+        }
+        ArrayAdapter<String> adap= new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                liste);
+
+        lv.setAdapter(adap);
+
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +45,12 @@ public class MainActivity extends AppCompatActivity {
 
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            public void onProgressChanged(SeekBar seekBar, int pos, boolean b) {
 
-                Log.i("SeekBarValue",String.valueOf(i));
+
+                if(sb.getProgress()==0)
+                    sb.setProgress(1);
+                update();
             }
 
             @Override
@@ -39,6 +63,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
+update();
     }
 }
